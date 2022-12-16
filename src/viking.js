3f68c59d00c1a1faf0cc3c94077ba2fb7e91d974
Dani Calvente <danicalvente@hotmail.com>
2022-12-16 19:28:55 +0100

    Iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,86 +1,25 @@
 // Soldier
 class Soldier {
-  constructor(healthParam, strengthParam) {
-    this.health = healthParam;
-    this.strength = strengthParam;
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
   }
-  attack () {
-    return this.strength
-  }
-  receiveDamage ( damage ) {
-    this.health = this.health - damage
-  }
+  attack = () => {
+    return this.strength;
+  };
+  receiveDamage = (damage) => {
+    this.health -= damage;
+  };
 }
 
 // Viking
-class Viking extends Soldier {
-  constructor (nameParam, healthParam, strengthParam ) {
-    super (healthParam, strengthParam)
-    this.name= nameParam
-  }
-  receiveDamage (damage) {
-    this.health = this.health - damage;
-    if ( this.health > 0){
-      return `${this.name} has received ${damage} points of damage`
-    } else {
-      return `${this.name} has died in act of combat`
-    }
-  }
-
-  battleCry () {
-    return "Odin Owns You All!"
-  }
-  
-}
-
-
+class Viking {}
 
 // Saxon
-class Saxon extends Soldier {
-  constructor( healthParam, strengthParam ){
-    super (healthParam, strengthParam)
-
-  }
-
-  receiveDamage (damage) {
-    this.health = this.health - damage;
-    if ( this.health > 0){
-      return `A Saxon has received ${damage} points of damage`
-    } else {
-      return `A Saxon has died in combat`
-    }
-  }
-
-}
+class Saxon {}
 
 // War
-class War {
-  constructor () {
-    this.vikingArmy = [];
-    this.saxonArmy = [];
-  }
-
-  newViking = new Viking
-  addViking ( newViking ) {
-    this.vikingArmy.push (newViking)
-  }
-
-  newSaxon = new Saxon
-  addSaxon (newSaxon) {
-    this.saxonArmy.push (newSaxon)
-  }
-
-  vikingAttack ( ) {
-    
-  }
-
-  saxonAttack ( ) {
-
-  }
-
-}
-
-
+class War {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
